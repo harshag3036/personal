bb3d294f5ac6f97b5e14987b8be26c250cbde815
50019090 <harsh.agarwal@piramal.com>
2024-08-18 03:44:33 +0530

    Hello world

--- src/main/java/com/personal/PersonalApplication.java
+++ src/main/java/com/personal/PersonalApplication.java
@@ -2,10 +2,19 @@ package com.personal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class PersonalApplication {
 
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalApplication.class, args);
 	}
--- src/main/java/com/personal/controller/Heroku.java
+++ src/main/java/com/personal/controller/Heroku.java
@@ -1,19 +1,17 @@
 package com.personal.controller;
 
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(path = "/")
-@Slf4j
-@CrossOrigin
-public class Heroku {
-   @GetMapping
-    String home() {
-        return "Hello World!";
-    }
-}
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//
+//@Slf4j
+//@CrossOrigin
+//public class Heroku {
+//   @ResponseBody
+//   @RequestMapping(path = "/")
+//    String home() {
+//        return "Hello World!";
+//    }
+//}
